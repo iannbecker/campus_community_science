f21f106cb0877f8a0eeac689351d8e672b1e8dd4
Ian Becker <ianbeck018@gmail.com>
2025-12-29 11:07:17 -0600

    WIP - Most initial filtering complete

--- scripts/data_prep_cleaning/campus_data_pull.r
+++ scripts/data_prep_cleaning/campus_data_pull.r
@@ -36,3 +36,27 @@ campus_data <- ipeds_raw %>%
                                        "Doctor's degree"))
 
 # Filter to only columns of interest
+
+campus_data <- campus_data %>%
+  select(inst_name, state_abbr, county_name, offering_highest_level, 
+         offering_highest_degree, tribal_college, urban_centric_locale, offering_undergrad, 
+         offering_grad, inst_status, degree_granting, open_public, land_grant, inst_size,
+         longitude, latitude)
+
+# Filter out tribal schools, schools not offering undergrad, schools not offering degrees
+
+campus_data <- campus_data %>%
+  filter(tribal_college == "No",
+         offering_undergrad == "Yes",
+         degree_granting == "Yes")
+
+# Remove columns
+
+campus_data <- campus_data %>%
+  select(-tribal_college, -offering_undergrad, -degree_granting)
+
+# Remove any NA for institution size
+
+campus_data <- campus_data %>%
+  filter(!inst_size == "Not applicable")
+
